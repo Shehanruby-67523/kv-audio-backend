c5bf617b72ce0923e80c1b499fb5901224e79923
Shehan Perera <kurugamaperera2003@gmail.com>
2025-01-07 18:40:06 +0530

    plugged  and imported getReview function to reviewRouter.js 😜

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { addReview } from '../controllers/reviewController.js';
+import { addReview, getReviews } from '../controllers/reviewController.js';
 
 const reviewRouter = express.Router();
 reviewRouter.post("/",addReview);
+reviewRouter.get("/",getReviews);
 
 export default reviewRouter;
