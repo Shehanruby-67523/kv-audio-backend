1fa709809aed5dae5c6bd5c996cc7ac53000e09a
Shehan Perera <kurugamaperera2003@gmail.com>
2025-01-07 18:16:35 +0530

    created the get request for users and admins 👀

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -22,3 +22,21 @@ export function addReview(req,res){
     res.status(500).json({error: "Review could not be added"});
   })
 }
+
+export function getReviews(req,res){
+
+  const user = req.user;
+
+  if(user == null || user.role != "admin"){
+    Review.find({isApproved: true}).then((reviews)=>{
+      res.json(reviews);
+    })
+    return;
+  } 
+
+  if(user.role == "admin"){
+    Review.find().then((reviews)=>{
+      res.json(reviews);
+    })
+  }
+}
