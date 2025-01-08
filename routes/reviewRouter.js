f785281f5d246e34009bc8ba3adc443d026113f8
Shehan Perera <kurugamaperera2003@gmail.com>
2025-01-08 17:36:21 +0530

    added plugs to identify whether the get reguest is approved rute or an email route 🎁

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -5,5 +5,15 @@ const reviewRouter = express.Router();
 reviewRouter.post("/",addReview);
 reviewRouter.get("/",getReviews);
 reviewRouter.delete("/:email",deleteReview);
+reviewRouter.get("/approved",
+  (req,res)=>{
+    console.log("This is approved route")
+  }
+)
+reviewRouter.get("/:email",
+  (req,res)=>{
+    console.log("This is email route")
+  }
+)
 
 export default reviewRouter;
