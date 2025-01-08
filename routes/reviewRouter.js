a2ab20e6bbe38aa889be526725cabe97f825b1e1
Shehan Perera <kurugamaperera2003@gmail.com>
2025-01-08 22:24:26 +0530

    renamed route for approving reviews to improve clarity 🎁

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -5,7 +5,7 @@ const reviewRouter = express.Router();//This is the router for the review
 reviewRouter.post("/",addReview);//This route is for adding a review
 reviewRouter.get("/",getReviews);//This route is for getting the reviews
 reviewRouter.delete("/:email",deleteReview);//This route needs the email to delete the review
-reviewRouter.put("/approved/:email",approveReview);//This route needs both approved and the email to the update the review
+reviewRouter.put("/approve/:email",approveReview);//This route needs both approved and the email to the update the review
 
 // reviewRouter.get("/approved",
 //   (req,res)=>{
