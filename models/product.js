f8a634181394973afd23fa957dd3330448f5d681
Shehan Perera <kurugamaperera2003@gmail.com>
2025-01-14 21:41:17 +0530

    add unique key and dimensions fields to product schema in prodect.js ✌

--- models/product.js
+++ models/product.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
+  key : {
+    type : String,
+    required : true,
+    unique : true
+  },
   name : {
     type : String,
     required : true
   },
-
   price : {
     type : Number,
     required : true
   },
-
+  dimensions : {
+    type : String,
+    required : true
+  },
   description: {
     type : String,
     required : true
