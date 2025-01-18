6c1cd7288aa87a19d5f33068ddd47e7e0470bcb9
Shehan Perera <kurugamaperera2003@gmail.com>
2025-01-18 16:57:51 +0530

    added updateProduct route to productRouter.js 🙌

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { addProduct, getProducts } from "../controllers/productController.js";
+import { updateProducts } from "../controllers/usercontroller.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",addProduct);
 productRouter.get("/",getProducts);
+productRouter.put("/key",updateProducts);
 
 export default productRouter;
