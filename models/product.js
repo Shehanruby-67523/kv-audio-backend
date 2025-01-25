19d17d329062f9d8cc474604460576cd1b1da383
Shehan <kurugamaperera2003@gmail.com>
2025-01-25 11:53:17 +0530

    update default image URL in product schema (product.js) ✌

--- models/product.js
+++ models/product.js
@@ -36,7 +36,7 @@ const productSchema = new mongoose.Schema({
   image : {
     type : String,
     required : true,
-    default : "https://via.placeholder.com/150"
+    default : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
   }
 })
 
