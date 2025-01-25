af35763023563100cf20326622a826a75b1d476e
Shehan <kurugamaperera2003@gmail.com>
2025-01-25 12:24:16 +0530

    created inquiry.js file to send messages to admins by users 😎

---
+++ models/inquiry.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+// Schema for the inquiry
+const inquirySchema = new mongoose.Schema({
+    id:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    message:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    response:{
+        type: String,
+        required: false,
+        default: ""
+    },
+    isResolved:{
+        type: Boolean,
+        required: true,
+        default: false
+    }
+})
+
+// Model for the inquiry
+const Inquiry = mongoose.model("Inquiry",inquirySchema);
+
+export default Inquiry;
