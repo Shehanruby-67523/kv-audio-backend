c6fe703ac99f85cb15e3d610c4e9050252feffe6
Shehan <kurugamaperera2003@gmail.com>
2025-01-27 12:31:29 +0530

    added update and delete functions to the inquiryController file 😀

--- routes/inquiryRouter.js
+++ routes/inquiryRouter.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { addInquiry } from "../controllers/inquiryController.js";
+import { addInquiry, getInquiries, updateInquiry, deleteInquiry } from "../controllers/inquiryController.js";
 
 const inquiryRouter = express.Router();
 
 inquiryRouter.post("/", addInquiry);
+inquiryRouter.get("/", getInquiries);
+inquiryRouter.put("/:id", updateInquiry);
+inquiryRouter.delete("/:id", deleteInquiry);
 
 export default inquiryRouter;
